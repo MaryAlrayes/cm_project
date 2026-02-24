53c4a919e441a70be409ba926b48f93257c605de
Agnese <a.bertoncini1@studenti.unipi.it>
2026-02-24 15:30:54 +0100

    check missing values and dataset's shape

--- test1.py
+++ test1.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 
 # 2. Save it as a clean CSV for later use
-data=pd.read_csv('D:\cm\cm_project\dataset\diabetes_cleaned.csv', )
+data=pd.read_csv('dataset\diabetes_cleaned.csv')
 
 # 3. Quick check: see the first 5 rows
 print(data.head())
+
+total_null = data.isnull().sum().sum()
+print(f"There are {total_null} missing values")
+
+print(data.shape)
