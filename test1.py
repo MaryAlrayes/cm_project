a1734faa6e1149cb9b21903813518feb12445766
Mary Alrayes <m.alrayes@studenti.unipi.it>
2026-02-24 15:15:39 +0100

    reading data

---
+++ test1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+# 2. Save it as a clean CSV for later use
+data=pd.read_csv('D:\cm\cm_project\dataset\diabetes_cleaned.csv', )
+
+# 3. Quick check: see the first 5 rows
+print(data.head())
